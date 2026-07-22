29a3a47991c9280f90c636371ec0549e1bec4426
felipe-4-coder <aranegafelipe@gmail.com>
2026-07-22 18:42:08 -0300

    atualiza qc_filter.py

--- qc_filter.py
+++ qc_filter.py
@@ -13,18 +13,39 @@ def qc_filter():
         if arquivo.endswith(".zip"):
             print(arquivo)
             caminho_zip = os.path.join(data_dir, arquivo)
+
+            ano = arquivo.replace(".zip", "").split("_")[-1]
+            pasta_ano = os.path.join(approved_dir, ano)
+            os.makedirs(pasta_ano, exist_ok=True)
         
             try:
                 with zipfile.ZipFile(caminho_zip, "r") as z:
                     with z.open("ncbi_dataset/data/assembly_data_report.jsonl") as f:
+
                         conteudo = f.read()
+
+                        aprovados = set()
+
                         for linha in conteudo.decode("utf-8").splitlines():
                             dados = json.loads(linha)
+                            accession = dados.get("accession", "")
+    
+                            # pula se já processou esse accession
+                            if accession in aprovados:
+                                continue
+    
                             nivel = dados.get("assemblyInfo", {}).get("assemblyLevel", "")
                             n50 = dados.get("assemblyStats", {}).get("contigN50", 0)
                             tamanho = int(dados.get("assemblyStats", {}).get("totalSequenceLength", 0))
+    
                             if nivel in ["Complete Genome", "Chromosome"] and n50 > 50000 and 4500000 < tamanho < 6500000:
-                                print(f"  ✓ APROVADO: {dados.get('accession', 'N/A')}")
+                                aprovados.add(accession)
+                            # extrai FASTA...
+
+                            for nome_arquivo in z.namelist():
+                                if accession in nome_arquivo and nome_arquivo.endswith(".fna"):
+                                    z.extract(nome_arquivo, pasta_ano)
+                                    print(f"  ✓ APROVADO e extraído: {accession}")
                             else:
                                 print(f"  ✗ REPROVADO: {dados.get('accession', 'N/A')}")
             except zipfile.BadZipFile:
